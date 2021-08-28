c03bffac553e76312b1274df2bc9139eea874b1f
PrinceNarteh <prinarteh@gmail.com>
2021-08-28 20:21:29 +0000

    Add sqlite database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { User } from './auth/user.entity';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [User],
+      synchronize: true,
+    }),
+    AuthModule,
+  ],
 })
 export class AppModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { User } from './user.entity';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private authRepository: Repository<User>,
+  ) {}
+
   create(body: CreateUserDto) {
-    return body;
+    const user = this.authRepository.create(body);
+    return this.authRepository.save(user);
   }
 
   findUser() {
