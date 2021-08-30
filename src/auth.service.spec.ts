6e56ea5f88aa3f2269727b9c075722bbaaf56522
PrinceNarteh <prinarteh@gmail.com>
2021-08-30 20:47:48 +0000

    Add test to throw an error if email is already in use

--- src/auth.service.spec.ts
+++ src/auth.service.spec.ts
@@ -6,9 +6,17 @@ import { UsersService } from './auth/users.service';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let fakeUsersService: Partial<UsersService>;
+  let fakeUser: User = {
+    id: 1,
+    firstName: 'Jane',
+    lastName: 'Smith',
+    email: 'jane.smith@email.com',
+    password: 'password',
+  };
 
   beforeEach(async () => {
-    const fakeUsersService: Partial<UsersService> = {
+    fakeUsersService = {
       find: () => Promise.resolve([]),
       create: (body: CreateUserDto) =>
         Promise.resolve({ id: 1, ...body } as User),
@@ -32,16 +40,19 @@ describe('AuthService', () => {
   });
 
   it('creates a new user with a salted and hashed password', async () => {
-    const user = await service.register({
-      firstName: 'Jane',
-      lastName: 'Smith',
-      email: 'jane.smith@email.com',
-      password: 'password',
-    });
+    const user = await service.register(fakeUser);
 
     expect(user.password).not.toEqual('password');
     const [hash, salt] = user.password.split('.');
     expect(hash).toBeDefined();
     expect(salt).toBeDefined();
+  });
+
+  it('throws an error if user signs up with email that is in use', async () => {
+    fakeUsersService.find = () => Promise.resolve([fakeUser]);
+
+    try {
+      await service.register(fakeUser);
+    } catch (error) {}
   });
 });
