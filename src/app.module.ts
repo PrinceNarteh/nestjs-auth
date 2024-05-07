0f8649619a1989682a19c31320ef59bffadec562
Prince Narteh <prinarteh@gmail.com>
2024-05-07 16:19:19 -0700

    feat: add users module and entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { EnvModule } from './env/env.module';
 import { DatabaseModule } from './database/database.module';
 import { IamModule } from './modules/iam/iam.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [EnvModule, DatabaseModule, IamModule],
+  imports: [EnvModule, DatabaseModule, IamModule, UsersModule],
   controllers: [],
   providers: [],
 })
