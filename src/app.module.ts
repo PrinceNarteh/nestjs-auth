b183b42d583ebbda77f3261eb0252cfa343c34a0
Prince Narteh <prinarteh@gmail.com>
2024-05-07 04:55:22 -0700

    feat: add hashing service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { EnvModule } from './env/env.module';
 import { DatabaseModule } from './database/database.module';
+import { IamModule } from './modules/iam/iam.module';
 
 @Module({
-  imports: [EnvModule, DatabaseModule],
+  imports: [EnvModule, DatabaseModule, IamModule],
   controllers: [],
   providers: [],
 })
---
+++ src/modules/iam/hashing/bcrypt.service.ts
@@ -0,0 +1,15 @@
+import { compare, genSalt, hash } from 'bcrypt';
+import { HashingService } from './hashing.service';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class BcryptService implements HashingService {
+  async hash(data: string | Buffer): Promise<string> {
+    const salt = await genSalt();
+    return hash(data, salt);
+  }
+
+  async compare(data: string | Buffer, encrypted: string): Promise<boolean> {
+    return compare(data, encrypted);
+  }
+}
