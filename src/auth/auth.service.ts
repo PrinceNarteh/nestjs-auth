8be1d3f533672762ea4e6cd07ac984a694ff75fd
Prince Narteh <prinarteh@gmail.com>
2026-05-20 20:34:02 +0000

    feat: add register service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,48 @@
-import { Inject } from '@nestjs/common';
-import { DRIZZLE } from 'src/database/database.module';
-import type { DrizzleDB } from 'src/database/types/drizzle';
+import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { EmailService } from './email.service';
+import { RegisterDTO } from './dto/register.dto';
+import { ConflictException } from '@nestjs/common';
+import * as bcrypt from 'bcryptjs';
+import * as crypto from 'crypto';
 
 export class AuthService {
   constructor(
-    @Inject(DRIZZLE)
-    private readonly db: DrizzleDB,
+    private userService: UsersService,
+    private jwtService: JwtService,
+    private configService: ConfigService,
+    private emailService: EmailService,
   ) {}
 
-  async login() {}
+  async register(dto: RegisterDTO) {
+    const userExists = await this.userService.findById(dto.email);
+    if (userExists) {
+      throw new ConflictException('An account with this email already exists');
+    }
 
-  async register() {}
+    const passwordHash = await bcrypt.hash(dto.password, 12);
+    const verificationToken = crypto.randomBytes(32).toString('hex');
+    const verificationTokenExpiresAt = new Date(
+      Date.now() + 20 * 60 * 60 * 1000,
+    );
+
+    const user = await this.userService.create({
+      firstName: dto.firstName,
+      lastName: dto.lastName,
+      email: dto.email,
+      passwordHash,
+      verificationToken,
+      verificationTokenExpiresAt,
+    });
+
+    void this.emailService.sendVerificationEmail(user.email, verificationToken);
+
+    return {
+      message:
+        'Registration Successful. Please check your email to verify your account.',
+    };
+  }
+
+  async login() {}
 }
