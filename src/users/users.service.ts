4499f517fed995fbef780f9accd573e4b7fb8bf0
Prince Narteh <prinarteh@gmail.com>
2026-05-20 18:25:37 +0000

    feat: add users service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Inject, Injectable } from '@nestjs/common';
 import { DRIZZLE } from '../database/database.module';
 import { CreateUser, users } from 'src/database/schemas/user.schema';
 import { type DrizzleDB } from 'src/database/types/drizzle';
+import { eq } from 'drizzle-orm';
 
 @Injectable()
 export class UsersService {
@@ -10,11 +11,37 @@ export class UsersService {
     private readonly db: DrizzleDB,
   ) {}
 
-  getUser() {
+  async findAll() {
     return this.db.query.users.findMany();
   }
 
-  createUser(user: CreateUser) {
-    return this.db.insert(users).values(user);
+  async findByEmail(email: string) {
+    return this.db.query.users.findFirst({
+      where: eq(users.email, email),
+    });
+  }
+
+  async findById(id: string) {
+    return this.db.query.users.findFirst({
+      where: eq(users.id, id),
+    });
+  }
+
+  async create(data: CreateUser) {
+    const [user] = await this.db.insert(users).values(data).returning();
+    return user;
+  }
+
+  async update(id: string, data: Partial<CreateUser>) {
+    const [user] = await this.db
+      .update(users)
+      .set({ ...data })
+      .where(eq(users.id, id))
+      .returning();
+    return user;
+  }
+
+  async delete(id: string) {
+    await this.db.delete(users).where(eq(users.id, id));
   }
 }
