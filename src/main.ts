b36b9e1509f40adf1a482dfd7462e1dea6dae226
Prince Narteh <prinarteh@gmail.com>
2026-05-20 17:30:20 +0000

    feat: add exception filters and swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,46 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import cookieParser from 'cookie-parser';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+  app.use(cookieParser());
+
+  // set global prefix
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  // add exception handlers
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  // setup Swagger
+  const config = new DocumentBuilder()
+    .setTitle('NestJS Auth API')
+    .setDescription('Complete authentication system')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/docs', app, document);
+
+  const port = configService.get<number>('PORT') ?? 3000;
+
+  await app.listen(port);
+
+  const BASE_URL = `http://localhost:${port}/api`;
+  console.log(`Application running on ${BASE_URL}`);
+  console.log(`Swagger docs at ${BASE_URL}/docs`);
 }
 bootstrap();
