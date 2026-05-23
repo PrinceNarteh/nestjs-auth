feed39285f875be2b8cbd2f97d691abb664c6b2d
Prince Narteh <prinarteh@gmail.com>
2026-05-23 06:37:46 +0000

    feat: add register, login and verify-email controller function

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,50 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Query,
+  Res,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDTO } from './dto/login.dto';
 import { RegisterDTO } from './dto/register.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Public } from 'src/common/decorators/public.decorator';
+import { type Response } from 'express';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @Post('/login')
-  async login(@Body() data: LoginDTO) {}
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: 'Login to receive access and refresh token' })
+  async login(
+    @Body() dto: LoginDTO,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    return this.authService.login(dto, res);
+  }
 
+  @Public()
   @Post('/register')
-  async register(@Body() data: RegisterDTO) {}
+  @ApiOperation({ summary: 'Register a new user' })
+  async register(@Body() data: RegisterDTO) {
+    return this.authService.register(data);
+  }
+
+  @Public()
+  @Get('/verify-email')
+  @ApiOperation({ summary: 'Verify email address and auth-login' })
+  async verifyEmail(
+    @Query('token') token: string,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    return this.authService.verifyEmail(token, res);
+  }
 }
---
+++ src/common/decorators/current-user.decorator.ts
@@ -0,0 +1,14 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { Request } from 'express';
+import { User } from 'src/database/schemas';
+
+type RequestWithUser = Request & {
+  user: User;
+};
+
+export const CurrentUser = createParamDecorator(
+  (_data: unknown, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest<RequestWithUser>();
+    return request.user;
+  },
+);
