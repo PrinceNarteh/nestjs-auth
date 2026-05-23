7ce84f6fbc537e2dd2c84346e60854922349c3fa
Prince Narteh <prinarteh@gmail.com>
2026-05-23 07:11:12 +0000

    feat: AuthModule imports

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { EmailService } from './email.service';
+import { JwtModule } from '@nestjs/jwt';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  providers: [AuthService],
-  controllers: [],
+  imports: [JwtModule.register({}), UsersModule],
+  providers: [EmailService, AuthService],
+  controllers: [AuthController],
   exports: [AuthService],
 })
 export class AuthModule {}
